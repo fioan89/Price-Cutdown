e15b1acc7167af0d5127b4f364a7e289d2b4a37e
prabirshrestha <mail@prabir.me>
2015-10-21 14:22:32 +0300

    Extract data from emag (title, price and url)

--- PriceCutdown/spiders/emag_spider.py
+++ PriceCutdown/spiders/emag_spider.py
@@ -1,3 +1,5 @@
+from PriceCutdown.items import EmagItem
+
 __author__ = 'fauri'
 
 import scrapy
@@ -11,8 +13,23 @@ class EmagSpider(scrapy.Spider):
     base_product_url = "http://www.emag.ro"
 
     def parse(self, response):
+        item = self.parse_page_content(response)
+        if item:
+            yield item
+
+        # search for links to follow
         for url in response.xpath('//form/h2/a/@href').extract():
             yield scrapy.Request(response.urljoin(url), callback=self.parse)
 
-    def parse_dir_contents(self, response):
-        pass
+
+    def parse_page_content(self, response):
+        '''
+        Parses the response and tries to extract the EmagItem instance
+        :return: a EmagInstance if found.
+        '''
+        for content in response.xpath('//div[@class="big-box" and @itemprop="offerDetails"]'):
+            item = EmagItem()
+            item['product_name'] = content.xpath('//div[@id="product-offer"]/div[@id="offer-title"]/h1/text()').extract()
+            item['product_price'] = content.xpath('//span[@itemprop="price" and @content]/@content').extract()
+            item['product_link'] = response.url
+            return item
