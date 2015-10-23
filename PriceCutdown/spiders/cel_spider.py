4c1001a1137193f0a4c7e605f8292e9b5a75e21e
prabirshrestha <mail@prabir.me>
2015-10-23 15:10:01 +0300

    New spider for cel.ro

---
+++ PriceCutdown/spiders/cel_spider.py
@@ -0,0 +1,40 @@
+__author__ = 'fauri'
+
+from PriceCutdown.items import ProductItem
+
+import scrapy
+
+
+class CelSpider(scrapy.Spider):
+    name = "cel"
+    allowed_domains = ["cel.ro"]
+    start_urls = ['http://www.cel.ro/laptop-laptopuri/apple/']
+    base_product_url = "http://www.cel.ro"
+
+    def parse(self, response):
+        item = self.parse_page_content(response)
+        if item:
+            yield item
+
+        # search for links to follow
+        for url in response.xpath('//div[@class="productlisting"]/div[@class="productListing-tot"]/div[@class="productListing-nume"]/h2/a/@href').extract():
+            yield scrapy.Request(url, callback=self.parse)
+
+        # search for next page
+        for url in response.xpath('//div[@class="pageresults"]/a[text()=">>"]/@href').extract():
+            yield scrapy.Request(url, callback=self.parse)
+
+
+
+    def parse_page_content(self, response):
+        '''
+        Parses the response and tries to extract the EmagItem instance
+        :return: a EmagInstance if found.
+        '''
+        for content in response.xpath('//div[@class="prod_info"]'):
+            item = ProductItem()
+            item['product_owner'] = 'cel.ro'
+            item['product_name'] = content.xpath('//div[@class="pageHeading"]/h2/text()').extract()
+            item['product_price'] = content.xpath('//div[@class="pret_info"]/div[@itemprop="offers"]/b/text()').extract()
+            item['product_link'] = response.url
+            return item
