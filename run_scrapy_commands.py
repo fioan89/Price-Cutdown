31f15149b13a546b381863c0ebc3964ee8f88080
prabirshrestha <mail@prabir.me>
2015-10-28 09:30:58 +0200

    No need to export the proxy if proxy middleware is used.

--- run_scrapy_commands.py
+++ run_scrapy_commands.py
@@ -8,7 +8,7 @@ from scrapy.cmdline import execute
 # this script is here just to get rid of scrapy crawl command during debuging
 # with this I can simply start the process via an IDE (like PyCharm) and then enable breakpoints in my code
 
-os.environ['http_proxy'] = "web-proxy.corp.hpecorp.net:8080"
+# os.environ['http_proxy'] = "proxy.corp.net:8080"
 
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r'(-script\.pyw|\.exe)?$', '', sys.argv[0])
